a9a464917769759c85ac8c041c494b6ab98cf924
IsaacHi1 <isaac.isaac.henrique@gmail.com>
2019-05-05 21:01:19 -0300

    Criando script automatizado da install do SDK e FLUTTER

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+cp -r Flutter/ ~/
+cp -r Android/ ~/
+
+mkdir -p ~/.android
+touch ~/.android/repositories.cfg
+
+sdkmanager=~/Android/Sdk/tools/bin/sdkmanager
+
+sdkmanager "build-tools;28.0.3" "platforms;android-28" "system-images;android-28;google_apis;x86_64" "sources;android-28"
+
+sleep 1
+
+sdkmanager --licenses
+
+# Export Path Variables in Bash or Zsh
+
+pathvar="
+export PATH=\$PATH:~/Flutter/bin
+export ANDROID_HOME=~/Android/Sdk
+export PATH=\$PATH:\$ANDROID_HOME/tools/bin
+export PATH=\$PATH:\$ANDROID_HOME/platform-tools
+"
+
+if [-f file="~/.zshrc"] then
+	pathvar >> ~/.zshrc
+	source ~/.zshrc
+else
+	pathvar >> ~/.bashrc
+	source ~/.bashrc
+fi
+
+# End export Path Variables in Bash or Zsh
+
+flutter doctor
